a07f6b0f48a94a81a34f57e865b2eedb669bd9e8
Hongmin Lee <107842030+webplusangels@users.noreply.github.com>
2025-06-16 16:36:02 +0900

    feat: 사용자 업데이트 로직 업데이트, delete_user 함수를 리팩토링
    - 사용자 업데이트 로직을 'JSON' 모드로 사용하도록 업데이트
    - delete_user는 User 모델을 사용하도록 업데이트

--- src/users/service.py
+++ src/users/service.py
@@ -1,4 +1,4 @@
-from typing import cast
+from typing import Sequence, cast
 
 from fastapi import HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -56,7 +56,22 @@ async def authenticate_user(
     return db_user
 
 
-async def get_user_profile(db: AsyncSession, db_user: models.User) -> models.User:
+async def get_all_users(
+    db: AsyncSession, skip: int = 0, limit: int = 100
+) -> Sequence[models.User]:
+    """
+    모든 사용자를 조회합니다.
+
+    :param db: 비동기 데이터베이스 세션
+    :param skip: 건너뛸 사용자 수
+    :param limit: 조회할 최대 사용자 수
+    :return: 사용자 모델 리스트
+    """
+    users = await crud.get_users(db=db, skip=skip, limit=limit)
+    return users
+
+
+async def get_user_profile(db_user: models.User) -> models.User:
     """
     사용자 ID로 사용자의 프로필을 조회합니다.
 
@@ -64,7 +79,7 @@ async def get_user_profile(db: AsyncSession, db_user: models.User) -> models.Use
     :param user_id: 조회할 사용자 ID
     :return: 사용자 모델
     """
-    return await crud.get_user(db=db, user_id=cast(str, db_user.id))
+    return db_user
 
 
 async def update_user_profile(
@@ -90,8 +105,16 @@ async def deactivate_user(db: AsyncSession, db_user: models.User) -> models.User
 
     :param db: 비동기 데이터베이스 세션
     :param db_user: 데이터베이스에서 조회된 사용자 모델
+    :param current_user: 요청을 보낸 사용자 모델 (예: 관리자 확인용) # 추가 예시
     :return: 비활성화된 사용자 모델
     """
+    # if db_user.id == current_user.id:
+    #     raise HTTPException(status_code=403, detail="자기 자신을 비활성화할 수 없습니다.")
+
+    # # 예시: 관리자가 아닌데 다른 사람을 비활하려는 경우
+    # if not current_user.is_admin and db_user.id != current_user.id:
+    #     raise HTTPException(status_code=403, detail="다른 사용자를 비활성화할 권한이 없습니다.")
+
     deactivated_user = await crud.deactivate_user(db=db, db_user=db_user)
     return deactivated_user
 
@@ -104,7 +127,7 @@ async def delete_user(db: AsyncSession, db_user: models.User) -> None:
     :param db_user: 데이터베이스에서 조회된 사용자 모델
     :return: None
     """
-    await crud.delete_user(db=db, user_id=cast(str, db_user.id))
+    await crud.delete_user(db=db, db_user=db_user)
 
 
 async def update_admin_status(
