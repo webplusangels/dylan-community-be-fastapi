1cf4bfbe9054e37db73c3ca3da4171059a332f74
Hongmin Lee <107842030+webplusangels@users.noreply.github.com>
2025-07-03 16:06:23 +0900

    feat: asynccontextmanager 데코레이터를 통해 명시적으로 관리

--- src/db/session.py
+++ src/db/session.py
@@ -1,3 +1,4 @@
+from contextlib import asynccontextmanager
 from typing import AsyncGenerator
 
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -5,6 +6,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from src.db.base import AsyncSessionLocal
 
 
+@asynccontextmanager
 async def get_async_db() -> AsyncGenerator[AsyncSession, None]:
     """
     비동기 데이터베이스 세션을 생성하고 변환하는 제너레이터 함수입니다.
