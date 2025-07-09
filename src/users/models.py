025c3b818413b4a704c8bc516259dedb4746f578
Hongmin Lee <107842030+webplusangels@users.noreply.github.com>
2025-07-09 17:16:28 +0900

    feat: 사용자 모델에 활성화된 사용자 쿼리 및 Eager Loading 메서드 추가

--- src/users/models.py
+++ src/users/models.py
@@ -2,8 +2,8 @@ import uuid
 from datetime import datetime
 from typing import TYPE_CHECKING
 
-from sqlalchemy import Boolean, DateTime, Integer, String
-from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import Boolean, DateTime, Index, Integer, String, select
+from sqlalchemy.orm import Mapped, mapped_column, relationship, selectinload
 from sqlalchemy.sql import func
 
 from src.db.base import Base
@@ -26,6 +26,8 @@ class User(Base):
         "hashed_password",
         "token_version",
     }
+
+    __table_args__ = (Index("idx_user_active_deleted", "is_active", "deleted_at"),)
 
     id: Mapped[str] = mapped_column(
         String(36), primary_key=True, default=lambda: str(uuid.uuid4())
@@ -56,3 +58,55 @@ class User(Base):
     comments: Mapped[list["PostComment"]] = relationship(
         "PostComment", back_populates="author"
     )
+
+    @classmethod
+    def active_query(cls):
+        """
+        활성화된 사용자만 조회하는 쿼리입니다.
+        deleted_at이 None이고 is_active가 True인 사용자만 반환합니다.
+
+        :return: 활성화된 사용자를 조회하는 SQLAlchemy 쿼리 객체
+        """
+        return select(cls).where(cls.deleted_at.is_(None), cls.is_active.is_(True))
+
+    @classmethod
+    def with_posts(cls, stmt):
+        """
+        사용자와 작성한 게시글 정보를 eager loading하는 쿼리입니다.
+
+        :param stmt: SQLAlchemy 쿼리 객체
+        :return: 작성한 게시글 정보를 포함한 사용자 쿼리 객체
+        """
+        return stmt.options(selectinload(cls.posts))
+
+    @classmethod
+    def with_likes(cls, stmt):
+        """
+        사용자와 좋아요한 게시글 정보를 eager loading하는 쿼리입니다.
+
+        :param stmt: SQLAlchemy 쿼리 객체
+        :return: 좋아요한 게시글 정보를 포함한 사용자 쿼리 객체
+        """
+        return stmt.options(selectinload(cls.likes))
+
+    @classmethod
+    def with_comments(cls, stmt):
+        """
+        사용자와 작성한 댓글 정보를 eager loading하는 쿼리입니다.
+
+        :param stmt: SQLAlchemy 쿼리 객체
+        :return: 작성한 댓글 정보를 포함한 사용자 쿼리 객체
+        """
+        return stmt.options(selectinload(cls.comments))
+
+    @classmethod
+    def with_all(cls, stmt):
+        """
+        사용자와 작성한 게시글, 좋아요한 게시글, 작성한 댓글 정보를 모두 eager loading하는 쿼리입니다.
+
+        :param stmt: SQLAlchemy 쿼리 객체
+        :return: 모든 관련 정보를 포함한 사용자 쿼리 객체
+        """
+        return stmt.options(
+            selectinload(cls.posts), selectinload(cls.likes), selectinload(cls.comments)
+        )
